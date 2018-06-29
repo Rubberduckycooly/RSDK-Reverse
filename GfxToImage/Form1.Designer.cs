b18f8748da048f09b449dff9046d23cae3aadbdd
Rubberduckycooly <rubberduckycooly@outlook.com>
2018-06-29 22:37:23 +1000

    Added FULL Support for Creating AND Exporting .gfx files

--- GfxToImage/Form1.Designer.cs
+++ GfxToImage/Form1.Designer.cs
@@ -29,16 +29,26 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.ExportButton = new System.Windows.Forms.Button();
-            this.SourceFileLocation = new System.Windows.Forms.TextBox();
+            this.ExportToGFX = new System.Windows.Forms.Button();
+            this.SourceIMGLocation = new System.Windows.Forms.TextBox();
+            this.SelectGIFButton = new System.Windows.Forms.Button();
+            this.ExportIMGButton = new System.Windows.Forms.Button();
+            this.SourceGFXLocation = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.GFX2IMGLabel = new System.Windows.Forms.Label();
+            this.IMG2GFXLabel = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.ExportButton);
-            this.panel1.Controls.Add(this.SourceFileLocation);
+            this.panel1.Controls.Add(this.IMG2GFXLabel);
+            this.panel1.Controls.Add(this.GFX2IMGLabel);
+            this.panel1.Controls.Add(this.ExportToGFX);
+            this.panel1.Controls.Add(this.SourceIMGLocation);
+            this.panel1.Controls.Add(this.SelectGIFButton);
+            this.panel1.Controls.Add(this.ExportIMGButton);
+            this.panel1.Controls.Add(this.SourceGFXLocation);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -46,23 +56,51 @@
             this.panel1.Size = new System.Drawing.Size(453, 310);
             this.panel1.TabIndex = 0;
             // 
-            // ExportButton
+            // ExportToGFX
             // 
-            this.ExportButton.Location = new System.Drawing.Point(9, 101);
-            this.ExportButton.Name = "ExportButton";
-            this.ExportButton.Size = new System.Drawing.Size(117, 33);
-            this.ExportButton.TabIndex = 2;
-            this.ExportButton.Text = "Export";
-            this.ExportButton.UseVisualStyleBackColor = true;
-            this.ExportButton.Click += new System.EventHandler(this.ExportButton_Click);
+            this.ExportToGFX.Location = new System.Drawing.Point(9, 257);
+            this.ExportToGFX.Name = "ExportToGFX";
+            this.ExportToGFX.Size = new System.Drawing.Size(117, 33);
+            this.ExportToGFX.TabIndex = 5;
+            this.ExportToGFX.Text = "Export To GFX";
+            this.ExportToGFX.UseVisualStyleBackColor = true;
+            this.ExportToGFX.Click += new System.EventHandler(this.ExportToGFX_Click);
             // 
-            // SourceFileLocation
+            // SourceIMGLocation
             // 
-            this.SourceFileLocation.Location = new System.Drawing.Point(132, 48);
-            this.SourceFileLocation.Name = "SourceFileLocation";
-            this.SourceFileLocation.ReadOnly = true;
-            this.SourceFileLocation.Size = new System.Drawing.Size(265, 22);
-            this.SourceFileLocation.TabIndex = 1;
+            this.SourceIMGLocation.Location = new System.Drawing.Point(132, 209);
+            this.SourceIMGLocation.Name = "SourceIMGLocation";
+            this.SourceIMGLocation.ReadOnly = true;
+            this.SourceIMGLocation.Size = new System.Drawing.Size(265, 22);
+            this.SourceIMGLocation.TabIndex = 4;
+            // 
+            // SelectGIFButton
+            // 
+            this.SelectGIFButton.Location = new System.Drawing.Point(9, 204);
+            this.SelectGIFButton.Name = "SelectGIFButton";
+            this.SelectGIFButton.Size = new System.Drawing.Size(117, 33);
+            this.SelectGIFButton.TabIndex = 3;
+            this.SelectGIFButton.Text = "Select IMG File";
+            this.SelectGIFButton.UseVisualStyleBackColor = true;
+            this.SelectGIFButton.Click += new System.EventHandler(this.SelectGIFButton_Click);
+            // 
+            // ExportIMGButton
+            // 
+            this.ExportIMGButton.Location = new System.Drawing.Point(9, 101);
+            this.ExportIMGButton.Name = "ExportIMGButton";
+            this.ExportIMGButton.Size = new System.Drawing.Size(117, 33);
+            this.ExportIMGButton.TabIndex = 2;
+            this.ExportIMGButton.Text = "Export To IMG";
+            this.ExportIMGButton.UseVisualStyleBackColor = true;
+            this.ExportIMGButton.Click += new System.EventHandler(this.ExportButton_Click);
+            // 
+            // SourceGFXLocation
+            // 
+            this.SourceGFXLocation.Location = new System.Drawing.Point(132, 53);
+            this.SourceGFXLocation.Name = "SourceGFXLocation";
+            this.SourceGFXLocation.ReadOnly = true;
+            this.SourceGFXLocation.Size = new System.Drawing.Size(265, 22);
+            this.SourceGFXLocation.TabIndex = 1;
             // 
             // button1
             // 
@@ -74,6 +112,26 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // GFX2IMGLabel
+            // 
+            this.GFX2IMGLabel.AutoSize = true;
+            this.GFX2IMGLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GFX2IMGLabel.Location = new System.Drawing.Point(13, 13);
+            this.GFX2IMGLabel.Name = "GFX2IMGLabel";
+            this.GFX2IMGLabel.Size = new System.Drawing.Size(183, 25);
+            this.GFX2IMGLabel.TabIndex = 6;
+            this.GFX2IMGLabel.Text = "Export .gfx to image";
+            // 
+            // IMG2GFXLabel
+            // 
+            this.IMG2GFXLabel.AutoSize = true;
+            this.IMG2GFXLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IMG2GFXLabel.Location = new System.Drawing.Point(4, 176);
+            this.IMG2GFXLabel.Name = "IMG2GFXLabel";
+            this.IMG2GFXLabel.Size = new System.Drawing.Size(181, 25);
+            this.IMG2GFXLabel.TabIndex = 7;
+            this.IMG2GFXLabel.Text = "Import image to .gfx";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -81,7 +139,7 @@
             this.ClientSize = new System.Drawing.Size(453, 310);
             this.Controls.Add(this.panel1);
             this.Name = "MainForm";
-            this.Text = "GfxToImage";
+            this.Text = "GFX Tool";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -91,9 +149,14 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox SourceFileLocation;
+        private System.Windows.Forms.TextBox SourceGFXLocation;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button ExportButton;
+        private System.Windows.Forms.Button ExportIMGButton;
+        private System.Windows.Forms.Button ExportToGFX;
+        private System.Windows.Forms.TextBox SourceIMGLocation;
+        private System.Windows.Forms.Button SelectGIFButton;
+        private System.Windows.Forms.Label IMG2GFXLabel;
+        private System.Windows.Forms.Label GFX2IMGLabel;
     }
 }
 
--- GfxToImage/MainForm.cs
+++ GfxToImage/MainForm.cs
@@ -17,9 +17,17 @@ namespace GfxToImage
 
         bool dcGFX = false;
 
+        RSDKv1.gfx GFX;
+        System.Drawing.Imaging.ColorPalette GFXPal;
+
+        Bitmap IMG;
+        System.Drawing.Imaging.ColorPalette IMGPal;
+
         public MainForm()
         {
             InitializeComponent();
+            GFX = new RSDKv1.gfx();
+            IMG = new Bitmap(1,1);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -31,42 +39,90 @@ namespace GfxToImage
             if (dlg.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
             {
                 filename = dlg.FileName;
-                SourceFileLocation.Text = dlg.FileName;
+                SourceGFXLocation.Text = dlg.FileName;
                 if (dlg.FilterIndex-1 == 1)
                 {
                     dcGFX = true;
                 }
+                GFX = new RSDKv1.gfx(filename, dcGFX);
             }
         }
 
         private void ExportButton_Click(object sender, EventArgs e)
         {
-
             SaveFileDialog dlg = new SaveFileDialog();
             dlg.DefaultExt = ".GIF";
-            dlg.Filter = "PNG|*.png|GIF|*.gif|.bmp|*.bmp";
+            dlg.Filter = "PNG|*.png|GIF|*.gif|Bitmap Image|*.bmp";
 
             if (dlg.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
             {
-                    RSDKv1.gfx old = new RSDKv1.gfx(filename,dcGFX);
+                    //RSDKv1.gfx old = new RSDKv1.gfx(filename,dcGFX);
 
                     switch (dlg.FilterIndex - 1)
                     {
                         case 0:
-                            old.export(dlg.FileName, System.Drawing.Imaging.ImageFormat.Png);
-                            break;
+                        string txt0 = this.Text;
+                        this.Text = "Exporting - " + txt0;
+                        GFX.export(dlg.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                        this.Text = txt0;
+                        break;
                         case 1:
-                            old.export(dlg.FileName, System.Drawing.Imaging.ImageFormat.Gif);
-                            break;
+                        string txt1 = this.Text;
+                        this.Text = "Exporting - " + txt1;
+                        GFX.export(dlg.FileName, System.Drawing.Imaging.ImageFormat.Gif);
+                        this.Text = txt1;
+                        break;
                         case 2:
-                            old.export(dlg.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
-                            break;
+                        string txt2 = this.Text;
+                        this.Text = "Exporting - " + txt2;
+                        GFX.export(dlg.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                        this.Text = txt2;
+                        break;
                         default:
-                            old.export(dlg.FileName, System.Drawing.Imaging.ImageFormat.Gif);
-                            break;
+                        string txt3 = this.Text;
+                        this.Text = "Exporting - " + txt3;
+                        GFX.export(dlg.FileName, System.Drawing.Imaging.ImageFormat.Gif);
+                        this.Text = txt3;
+                        break;
                     }
             }
 
+        }
+
+        private void SelectGIFButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".GIF";
+            dlg.Filter = "GIF|*.gif|Bitmap Image|*.bmp";
+
+            if (dlg.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+            {
+                filename = dlg.FileName;
+                SourceIMGLocation.Text = dlg.FileName;
+                IMG = (Bitmap)Image.FromFile(dlg.FileName).Clone();
+                GFX.importFromBitmap(IMG);
+            }
+        }
+
+        private void ExportToGFX_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.DefaultExt = ".gfx";
+            dlg.Filter = "Sage 2007 (PC Demo) Retro-Sonic Graphics Files|*.gfx|Dreamcast Demo Retro-Sonic Graphics Files|*.gfx";
+
+            if (dlg.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+            {
+                filename = dlg.FileName;
+                SourceGFXLocation.Text = dlg.FileName;
+                if (dlg.FilterIndex - 1 == 1)
+                {
+                    dcGFX = true;
+                }
+                string txt = this.Text;
+                this.Text = "Exporting - " + txt;
+                GFX.Write(dlg.FileName,dcGFX);
+                this.Text = txt;
+            }
         }
     }
 }
