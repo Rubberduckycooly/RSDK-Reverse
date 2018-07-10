44b7b45f5ceaf313f61024b9a3e1d8d9d6f03a89
thesupersonic16 <supersonic16bit@gmail.com>
2018-07-11 01:13:52 +1000

    Moved RetroEd's Main Window to use a native Menubar ad Changed some colouring

--- RetroED/MainForm.cs
+++ RetroED/MainForm.cs
@@ -19,144 +19,150 @@ namespace RetroED
 
         Tools.MapEditor.MainView OpenMapEditor()
         {
-            Tools.MapEditor.MainView frm = new Tools.MapEditor.MainView();
+            var frm = new Tools.MapEditor.MainView();
             frm.TopLevel = false;
             frm.ControlBox = false;
-            frm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            frm.FormBorderStyle = FormBorderStyle.None;
             frm.Dock = DockStyle.Fill;
-            var newTab = new TabPage();
+            var newTab = new TabPage(frm.Text);
             newTab.Controls.Add(frm);
-            this.TabControl.TabPages.Add(newTab);
-            this.TabControl.SelectedTab = newTab;
-            this.TabControl.SelectedTab.Text = frm.Text;
+            TabControl.TabPages.Add(newTab);
+            TabControl.SelectedTab = newTab;
             frm.Show();
             return frm;
         }
 
         Tools.ChunkMappingsEditor.MainForm OpenMappingsEditor()
         {
-            Tools.ChunkMappingsEditor.MainForm frm = new Tools.ChunkMappingsEditor.MainForm();
+            var frm = new Tools.ChunkMappingsEditor.MainForm();
             frm.TopLevel = false;
             frm.ControlBox = false;
-            frm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            frm.FormBorderStyle = FormBorderStyle.None;
             frm.Dock = DockStyle.Fill;
-            var newTab = new TabPage();
+            var newTab = new TabPage(frm.Text);
             newTab.Controls.Add(frm);
-            this.TabControl.TabPages.Add(newTab);
-            this.TabControl.SelectedTab = newTab;
-            this.TabControl.SelectedTab.Text = frm.Text;
+            TabControl.TabPages.Add(newTab);
+            TabControl.SelectedTab = newTab;
             frm.Show();
             return frm;
         }
 
         Tools.PaletteEditor.MainForm OpenPaletteEditor()
         {
-            Tools.PaletteEditor.MainForm frm = new Tools.PaletteEditor.MainForm();
+            var frm = new Tools.PaletteEditor.MainForm();
             frm.TopLevel = false;
             frm.ControlBox = false;
-            frm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            frm.FormBorderStyle = FormBorderStyle.None;
             frm.Dock = DockStyle.Fill;
-            var newTab = new TabPage();
+            var newTab = new TabPage(frm.Text);
             newTab.Controls.Add(frm);
-            this.TabControl.TabPages.Add(newTab);
-            this.TabControl.SelectedTab = newTab;
-            this.TabControl.SelectedTab.Text = frm.Text;
+            TabControl.TabPages.Add(newTab);
+            TabControl.SelectedTab = newTab;
             frm.Show();
             return frm;
         }
 
         Tools.GFXTool.MainForm OpenGFXTool()
         {
-            Tools.GFXTool.MainForm frm = new Tools.GFXTool.MainForm();
+            var frm = new Tools.GFXTool.MainForm();
             frm.TopLevel = false;
             frm.ControlBox = false;
-            frm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            frm.FormBorderStyle = FormBorderStyle.None;
             frm.Dock = DockStyle.Fill;
-            var newTab = new TabPage();
+            var newTab = new TabPage(frm.Text);
             newTab.Controls.Add(frm);
-            this.TabControl.TabPages.Add(newTab);
-            this.TabControl.SelectedTab = newTab;
-            this.TabControl.SelectedTab.Text = frm.Text;
+            TabControl.TabPages.Add(newTab);
+            TabControl.SelectedTab = newTab;
             frm.Show();
             return frm;
         }
 
         Tools.NexusDecrypt.MainForm OpenNexusDecryptTool()
         {
-            Tools.NexusDecrypt.MainForm frm = new Tools.NexusDecrypt.MainForm();
+            var frm = new Tools.NexusDecrypt.MainForm();
             frm.TopLevel = false;
             frm.ControlBox = false;
-            frm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            frm.FormBorderStyle = FormBorderStyle.None;
             frm.Dock = DockStyle.Fill;
-            var newTab = new TabPage();
+            var newTab = new TabPage(frm.Text);
             newTab.Controls.Add(frm);
-            this.TabControl.TabPages.Add(newTab);
-            this.TabControl.SelectedTab = newTab;
-            this.TabControl.SelectedTab.Text = frm.Text;
+            TabControl.TabPages.Add(newTab);
+            TabControl.SelectedTab = newTab;
             frm.Show();
             return frm;
         }
 
         Tools.RSDKUnpacker.MainForm OpenRSDKUnpacker()
         {
-            Tools.RSDKUnpacker.MainForm frm = new Tools.RSDKUnpacker.MainForm();
+            var frm = new Tools.RSDKUnpacker.MainForm();
             frm.TopLevel = false;
             frm.ControlBox = false;
-            frm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            frm.FormBorderStyle = FormBorderStyle.None;
             frm.Dock = DockStyle.Fill;
-            var newTab = new TabPage();
+            var newTab = new TabPage(frm.Text);
             newTab.Controls.Add(frm);
-            this.TabControl.TabPages.Add(newTab);
-            this.TabControl.SelectedTab = newTab;
-            this.TabControl.SelectedTab.Text = frm.Text;
+            TabControl.TabPages.Add(newTab);
+            TabControl.SelectedTab = newTab;
             frm.Show();
             return frm;
         }
 
-        private void animationEditorToolStripMenuItem_Click(object sender, EventArgs e)
+        private void MenuItem_RSDKUnpacker_Click(object sender, EventArgs e)
         {
-
+            OpenRSDKUnpacker();
         }
 
-        private void mapEditorToolStripMenuItem_Click(object sender, EventArgs e)
+        private void MenuItem_AnimationEditor_Click(object sender, EventArgs e)
+        {
+            // TODO: Add Animation Editor
+        }
+
+        private void MenuItem_MapEditor_Click(object sender, EventArgs e)
         {
             OpenMapEditor();
         }
 
-        private void paletteEditorToolStripMenuItem_Click(object sender, EventArgs e)
+        private void MenuItem_PaletteEditor_Click(object sender, EventArgs e)
         {
             OpenPaletteEditor();
         }
 
-        private void chunkEditorToolStripMenuItem_Click(object sender, EventArgs e)
+        private void MenuItem_ChunkEditor_Click(object sender, EventArgs e)
         {
             OpenMappingsEditor();
         }
 
-        private void GFXToolToolStripMenuItem_Click(object sender, EventArgs e)
+        private void MenuItem_GFXTool_Click(object sender, EventArgs e)
         {
             OpenGFXTool();
         }
 
-        private void NexusDecryptToolToolStripMenuItem_Click(object sender, EventArgs e)
+        private void MenuItem_NexusDecrypter_Click(object sender, EventArgs e)
         {
             OpenNexusDecryptTool();
         }
 
-        private void closeTabToolStripMenuItem_Click(object sender, EventArgs e)
+        private void MenuItem_CloseTab_Click(object sender, EventArgs e)
         {
-            if (TabControl.SelectedTab != null) TabControl.SelectedTab.Dispose();
+            if (TabControl.SelectedTab != null)
+                TabControl.TabPages.Remove(TabControl.SelectedTab);
         }
 
-        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        private void MenuItem_About_Click(object sender, EventArgs e)
         {
-            AboutForm af = new AboutForm();
-            af.ShowDialog();
+            new AboutForm().ShowDialog();
         }
 
-        private void rSDKUnpackerToolStripMenuItem_Click(object sender, EventArgs e)
+        private void TabControl_SelectedIndexChanged(object sender, EventArgs e)
         {
-            OpenRSDKUnpacker();
+            if (TabControl.SelectedTab.Text != null)
+                Text = $"RetroED - {TabControl.SelectedTab.Text}";
+            else
+                Text = $"RetroED";
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
         }
     }
 }
