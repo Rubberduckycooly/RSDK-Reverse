194d53c9f9a8fb960f4432c0f4aa58d681b990b5
CarJem Generations <Cwallacecs@gmail.com>
2019-02-11 10:22:33 -0500

    Small Change Urgently Required for Maniac Editor.

--- RSDKv5/StageTiles.cs
+++ RSDKv5/StageTiles.cs
@@ -25,25 +25,25 @@ namespace RSDKv5
         public readonly TileConfig Config;
         public readonly GIF CollisionA;
 
-        public StageTiles(string stage_directory, string palleteDir = null)
-        {
-            Image = new GIF(Path.Combine(stage_directory, "16x16Tiles.gif"), palleteDir);
-            IDImage = new GIF(Path.Combine(Environment.CurrentDirectory, "16x16Tiles_ID.gif"));
-            EditorImage = new GIF(Path.Combine(Environment.CurrentDirectory, "16x16Tiles_Edit.gif"));
-            if (File.Exists(Path.Combine(stage_directory, "TileConfig.bin")))
-            {
-                Config = new TileConfig(Path.Combine(stage_directory, "TileConfig.bin"));
-            }
+		public StageTiles(string stage_directory, string palleteDir = null)
+		{
+			Image = new GIF(Path.Combine(stage_directory, "16x16Tiles.gif"), palleteDir);
+			IDImage = new GIF(Environment.CurrentDirectory + "\\Resources\\Tile Overlays\\" + "16x16Tiles_ID.gif");
+			EditorImage = new GIF(Environment.CurrentDirectory + "\\Resources\\Tile Overlays\\" + "16x16Tiles_Edit.gif");
+			if (File.Exists(Path.Combine(stage_directory, "TileConfig.bin")))
+			{
+				Config = new TileConfig(Path.Combine(stage_directory, "TileConfig.bin"));
+			}
 
-        }
+		}
 
-        public StageTiles()
-        {
-            Image = new GIF(Path.Combine(Environment.CurrentDirectory, "16x16Tiles_ID.gif"));
-            Config = new TileConfig();
-        }
+		public StageTiles()
+		{
+			Image = new GIF(Path.Combine(Environment.CurrentDirectory, "16x16Tiles_ID.gif"));
+			Config = new TileConfig();
+		}
 
-        public void Write(string filename)
+		public void Write(string filename)
         {
             Bitmap write = Image.GetBitmap(new Rectangle(0,0,16,16384));
             write.Save(filename);
